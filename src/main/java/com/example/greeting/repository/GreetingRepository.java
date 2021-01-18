819f34c23235add895b62fa5004d8005a6451dd2
vijay300 <gnvijayk@gmail.com>
2021-01-18 16:41:31 +0530

    [Vijay] Added: git igonre

--- src/main/java/com/example/greeting/repository/GreetingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.greeting.repository;
-
-import com.example.greeting.model.Greeting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GreetingRepository extends JpaRepository<Greeting, Long> {
-}
